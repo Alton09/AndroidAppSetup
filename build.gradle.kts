a54b4d68f7e09d736f5c647d601eee3ed3141774
John Qualls <john.qualls@headspace.com>
2024-05-19 23:33:10 -0500

    Add spotless formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.hilt.plugin) apply false
+    alias(libs.plugins.spotless) apply true
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        target("**/*.kt")
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
 }
