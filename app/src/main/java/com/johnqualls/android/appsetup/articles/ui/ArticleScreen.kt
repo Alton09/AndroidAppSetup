5d536f5c0498d5040226f86dfea53829048bfb37
John Qualls <john.qualls@headspace.com>
2024-05-27 23:35:47 -0500

    Get images loading

--- app/src/main/java/com/johnqualls/android/appsetup/articles/ui/ArticleScreen.kt
+++ app/src/main/java/com/johnqualls/android/appsetup/articles/ui/ArticleScreen.kt
@@ -1,53 +1,64 @@
 package com.johnqualls.android.appsetup.articles.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.johnqualls.android.appsetup.core.ui.Presenter
 import com.johnqualls.android.appsetup.core.ui.Screen
+import com.johnqualls.android.appsetup.core.ui.theme.AppSetupTheme
 import com.johnqualls.android.appsetup.core.ui.theme.mediumSpacing
 import com.johnqualls.android.appsetup.core.ui.theme.smallSpacing
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun ArticleScreenLoadingPreview() {
-    ArticleScreen(ArticlePresenterFake()).Render(state = ArticleUiState(isLoading = true))
+    AppSetupTheme {
+        ArticleScreen(ArticlePresenterFake()).Render(state = ArticleUiState(isLoading = true))
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun ArticleScreenContentPreview() {
-    ArticleScreen(ArticlePresenterFake()).Render(
-        state = ArticleUiState(
-            isLoading = false,
-            articles = listOf(
-                Article(
-                    "Everything to Know About Bitcoin Pizza Day",
-                    "Vinamrata Chaturvedi, Quartz",
-                    "On May 22, 2010, a man in Florida paid 10,000 Bitcoin for pizza.Read more...",
-                    "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/98aec6479bad523f5c89763f4acf0cf9.jpg\"",
-                ),
-                Article(
-                    "Everything to Know About Bitcoin Pizza Day",
-                    "Vinamrata Chaturvedi, Quartz",
-                    "On May 22, 2010, a man in Florida paid 10,000 Bitcoin for pizza.Read more...",
-                    "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/98aec6479bad523f5c89763f4acf0cf9.jpg\"",
+    AppSetupTheme {
+        ArticleScreen(ArticlePresenterFake()).Render(
+            state = ArticleUiState(
+                isLoading = false,
+                articles = listOf(
+                    Article(
+                        "Everything to Know About Bitcoin Pizza Day",
+                        "Vinamrata Chaturvedi, Quartz",
+                        "On May 22, 2010, a man in Florida paid 10,000 Bitcoin for pizza.Read more...",
+                        "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/98aec6479bad523f5c89763f4acf0cf9.jpg\"",
+                    ),
+                    Article(
+                        "Everything to Know About Bitcoin Pizza Day",
+                        "Vinamrata Chaturvedi, Quartz",
+                        "On May 22, 2010, a man in Florida paid 10,000 Bitcoin for pizza.Read more...",
+                        "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/98aec6479bad523f5c89763f4acf0cf9.jpg\"",
+                    ),
                 ),
             ),
-        ),
-    )
+        )
+    }
 }
 
 class ArticleScreen(override val presenter: Presenter<ArticleUiEvent, ArticleUiState>) :
@@ -93,22 +104,38 @@ class ArticleScreen(override val presenter: Presenter<ArticleUiEvent, ArticleUiS
 
     @Composable
     private fun ArticleCard(article: Article) {
-        Card {
-            Text(
-                modifier = Modifier.padding(smallSpacing),
-                text = article.title ?: "No Title Found",
-                style = MaterialTheme.typography.headlineLarge,
-            )
-            Text(
-                modifier = Modifier.padding(smallSpacing),
-                text = article.author ?: "No Author Name Found",
-                style = MaterialTheme.typography.titleLarge,
-            )
-            Text(
-                modifier = Modifier.padding(smallSpacing),
-                text = article.description ?: "No Description Found",
-                style = MaterialTheme.typography.bodyLarge,
+        article.urlToImage?.let { image ->
+            AsyncImage(
+                model = image,
+                contentDescription = "Article Image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(mediumSpacing)
+                    .clip(RoundedCornerShape(16.dp)),
+                onError = {
+                    Log.e(
+                        "ArticleScreen",
+                        "Error loading image",
+                        it.result.throwable,
+                    )
+                },
             )
         }
+        Text(
+            modifier = Modifier.padding(horizontal = mediumSpacing, vertical = smallSpacing),
+            text = article.title ?: "No Title Found",
+            style = MaterialTheme.typography.headlineLarge,
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = mediumSpacing, vertical = smallSpacing),
+            text = article.author ?: "No Author Name Found",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = mediumSpacing, vertical = smallSpacing),
+            text = article.description ?: "No Description Found",
+            style = MaterialTheme.typography.bodyLarge,
+        )
     }
 }
